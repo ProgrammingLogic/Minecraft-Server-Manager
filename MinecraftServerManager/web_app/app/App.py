798f4c20f70f89ce89ff5ffe6e39802c588694e0
Jonathyn Stiverson <jstiverson2002@gmail.com>
2021-03-27 14:11:39 -0500

    Updated code to not use __init__ for the main app file.

--- MinecraftServerManager/web_app/app/App.py
+++ MinecraftServerManager/web_app/app/App.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .Config import Config
+from Config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -8,8 +8,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from .models import import_models
-from .routes import create_routes
+from models import import_models
+from routes import create_routes
 
 models = import_models(app, db)
 create_routes(app, db)
